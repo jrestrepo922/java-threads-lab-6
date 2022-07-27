e8c0ea783958c7738c60efc4687ea09ccd4a00dd
Juan Martin Restrepo <tinto922@gmail.com>
2022-07-27 09:16:04 -0400

    completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 import java.util.concurrent.*;
+import java.util.stream.LongStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // create an executor
-        ExecutorService executor; 
+        ExecutorService executor;
+        executor = Executors.newFixedThreadPool(3);
 
         while (scanner.hasNext()) {
             int num = scanner.nextInt();
             // submit tasks to your executor
+
+            PrimeLogger primeLogger = new PrimeLogger(num);
+            executor.execute(primeLogger);
         }
+        executor.shutdown();
+
     }
 }
 
+// Allows me to create a new threads
 class PrimeLogger implements Runnable {
     private final int num;
 
@@ -24,5 +32,21 @@ class PrimeLogger implements Runnable {
     @Override
     public void run() {
         // print num if it is prime
+        if(num >= 2){
+            Boolean isPrime = true;
+            // Check from 2 to n-1
+            for (int i = 2; i < num; i++)
+                if (num % i == 0)
+                    isPrime = false;
+            if(isPrime){
+                System.out.println("The number " + num + " is a prime number");
+            } else {
+                System.out.println("NOT A PRIME NUMBER");
+            }
+        } else {
+            System.out.println("NOT A PRIME NUMBER");
+        }
+
+
     }
 }
